992086980d305dbe467d7d44186e1f1017f56efd
Calvin Cheng <linchuan.cheng@gmail.com>
2012-08-15 15:39:28 +0800

    Initial commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='Fabric Colors',
+    version='0.1dev',
+    packages=['fabric_colors', ],
+    license='LICENSE',
+    description='Reusable fabric functions',
+    long_description=open('README.md').read(),
+    author='Calvin Cheng',
+    author_email='calvin@calvinx.com',
+    install_requires=[],
+)
