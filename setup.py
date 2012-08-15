8a9e15f7ef1d1a78fd78fea4fbd6d6fc9869588f
Calvin Cheng <linchuan.cheng@gmail.com>
2012-08-16 03:27:19 +0800

    Update setup.py to version 0.5

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='Fabric Colors',
-    version='0.4',
+    version='0.5',
     packages=['fabric_colors', ],
     license='LICENSE',
     description='Reusable fabric functions',
