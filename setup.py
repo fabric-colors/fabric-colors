16da3b9a44cdffbeab15b5ae8c04fe9acdffb677
Calvin Cheng <linchuan.cheng@gmail.com>
2012-09-16 21:14:15 +0800

    Correct the release version number in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='Fabric Colors',
-    version='0.8.5',
+    version='0.8.8',
     packages=['fabric_colors', ],
     license='LICENSE',
     description='Reusable fabric functions',
