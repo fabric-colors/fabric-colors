469b10933d07dddfa1694d6cc4f2bffdc559e768
Calvin Cheng <linchuan.cheng@gmail.com>
2013-04-17 17:57:14 +0800

    Bump version number to 0.9.23

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='Fabric Colors',
-    version='0.9.22',
+    version='0.9.23',
     packages=['fabric_colors', ],
     license='LICENSE',
     description='Reusable fabric functions',
