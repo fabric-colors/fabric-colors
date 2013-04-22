d2ae095ca6fe6ef02fd9dd67ca4dd058a98f4a30
Calvin Cheng <linchuan.cheng@gmail.com>
2013-04-22 15:33:16 +0800

    Bump to 0.9.36

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='Fabric Colors',
-    version='0.9.35',
+    version='0.9.36',
     packages=['fabric_colors', ],
     license='LICENSE',
     description='Reusable fabric functions',
